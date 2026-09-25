ea30f5ab30189b7408ea66ea52d1545913afc5bd
Erwin Victorio <victoriomark54@gmail.com>
2026-09-25 14:48:01 +0800

    fix

--- resources/js/Components/Dropdown.jsx
+++ resources/js/Components/Dropdown.jsx
@@ -1,5 +1,5 @@
 import { Transition } from '@headlessui/react';
-import { Link } from '@inertiajs/react';
+import { Link  } from '@inertiajs/react';
 import { createContext, useContext, useState } from 'react';
 
 const DropDownContext = createContext();
